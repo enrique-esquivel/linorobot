c5c9851c2042d4d1798d9e518c0879413bd2d43d
Enrique Esquivel <the_kique@hotmail.com>
2019-10-04 10:13:45 -0500

    Setup baseboard and robot parameters

--- teensy/firmware/lib/config/lino_base_config.h
+++ teensy/firmware/lib/config/lino_base_config.h
@@ -14,24 +14,24 @@
 // #define USE_ESC
 
 //uncomment the IMU you're using
-#define USE_GY85_IMU
-// #define USE_MPU6050_IMU
+//#define USE_GY85_IMU
+#define USE_MPU6050_IMU
 // #define USE_MPU9150_IMU
 // #define USE_MPU9250_IMU
 
 #define DEBUG 1
 
-#define K_P 0.6 // P constant
-#define K_I 0.3 // I constant
-#define K_D 0.5 // D constant
+#define K_P 0.18 // P constant
+#define K_I 0.0 // I constant
+#define K_D 0.1 // D constant
 
 //define your robot' specs here
-#define MAX_RPM 330               // motor's maximum RPM
-#define COUNTS_PER_REV 1550       // wheel encoder's no of ticks per rev
-#define WHEEL_DIAMETER 0.10       // wheel's diameter in meters
+#define MAX_RPM 185               // motor's maximum RPM
+#define COUNTS_PER_REV 368        // wheel encoder's no of ticks per rev
+#define WHEEL_DIAMETER 0.07       // wheel's diameter in meters
 #define PWM_BITS 8                // PWM Resolution of the microcontroller
-#define LR_WHEELS_DISTANCE 0.235  // distance between left and right wheels
-#define FR_WHEELS_DISTANCE 0.30   // distance between front and rear wheels. Ignore this if you're on 2WD/ACKERMANN
+#define LR_WHEELS_DISTANCE 0.10   // distance between left and right wheels
+#define FR_WHEELS_DISTANCE 0.13   // distance between front and rear wheels. Ignore this if you're on 2WD/ACKERMANN
 #define MAX_STEERING_ANGLE 0.415  // max steering angle. This only applies to Ackermann steering
 
 //=================BIGGER ROBOT SPEC (BTS7960)=============================
@@ -56,38 +56,40 @@ ROBOT ORIENTATION
          BACK
 */
 
+#define NC (0)
+
 /// ENCODER PINS
-#define MOTOR1_ENCODER_A 15
-#define MOTOR1_ENCODER_B 14 
+#define MOTOR1_ENCODER_A 19
+#define MOTOR1_ENCODER_B 42 
 
-#define MOTOR2_ENCODER_A 11
-#define MOTOR2_ENCODER_B 12 
+#define MOTOR2_ENCODER_A 18
+#define MOTOR2_ENCODER_B 43 
 
-#define MOTOR3_ENCODER_A 17
-#define MOTOR3_ENCODER_B 16 
+#define MOTOR3_ENCODER_A NC
+#define MOTOR3_ENCODER_B NC 
 
-#define MOTOR4_ENCODER_A 9
-#define MOTOR4_ENCODER_B 10
+#define MOTOR4_ENCODER_A NC
+#define MOTOR4_ENCODER_B NC
 
 //MOTOR PINS
 #ifdef USE_L298_DRIVER
   #define MOTOR_DRIVER L298
 
-  #define MOTOR1_PWM 21
-  #define MOTOR1_IN_A 20
-  #define MOTOR1_IN_B 1
+  #define MOTOR1_PWM 11
+  #define MOTOR1_IN_A 49
+  #define MOTOR1_IN_B 48
 
-  #define MOTOR2_PWM 5
-  #define MOTOR2_IN_A 8
-  #define MOTOR2_IN_B 6
+  #define MOTOR2_PWM 10
+  #define MOTOR2_IN_A 47
+  #define MOTOR2_IN_B 46
 
-  #define MOTOR3_PWM 22
-  #define MOTOR3_IN_A 23
-  #define MOTOR3_IN_B 0
+  #define MOTOR3_PWM NC
+  #define MOTOR3_IN_A NC
+  #define MOTOR3_IN_B NC
 
-  #define MOTOR4_PWM 4
-  #define MOTOR4_IN_A 2
-  #define MOTOR4_IN_B 3
+  #define MOTOR4_PWM NC
+  #define MOTOR4_IN_A NC
+  #define MOTOR4_IN_B NC
 
   #define PWM_MAX pow(2, PWM_BITS) - 1
   #define PWM_MIN -PWM_MAX
